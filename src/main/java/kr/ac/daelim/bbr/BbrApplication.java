8a9ed4c0a887eb9f74c147709dbf5947c22be0fb
Kim Juho <sv05159@naver.com>
2021-09-13 16:02:56 +0900

    Initial commit

---
+++ src/main/java/kr/ac/daelim/bbr/BbrApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.daelim.bbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/daelim/bbr/BbrApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.daelim.bbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
