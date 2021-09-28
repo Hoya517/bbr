cf8cb58c5a0740e70a376a6e5b73aec5c78032d4
Kim Juho <sv05159@naver.com>
2021-09-28 23:47:18 +0900

    login session

--- src/main/java/kr/ac/daelim/bbr/web/HomeController.java
+++ src/main/java/kr/ac/daelim/bbr/web/HomeController.java
@@ -1,13 +1,21 @@
 package kr.ac.daelim.bbr.web;
 
+import kr.ac.daelim.bbr.domain.member.Member;
+import kr.ac.daelim.bbr.web.argumentresolver.Login;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@Login Member loginMember, Model model) {
+
+        if (loginMember != null) {
+            model.addAttribute("member", loginMember);
+        }
+
         return "index";
     }
 
