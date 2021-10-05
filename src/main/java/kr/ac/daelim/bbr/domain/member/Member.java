2476dcdbd5b6b090cb2c02f2d99fc249677fc3ba
Kim Juho <sv05159@naver.com>
2021-10-06 02:40:32 +0900

    사용자 필드 추가

--- src/main/java/kr/ac/daelim/bbr/domain/member/Member.java
+++ src/main/java/kr/ac/daelim/bbr/domain/member/Member.java
@@ -22,8 +22,11 @@ public class Member extends BaseTimeEntity {
     private String phone;
     private String birth;
     private String email;
+    private String code;
     private String username;
     private String password;
+    private String department;
+    private String emailAuthYn;
     private String personalInfoTermYn;
     private String serviceTermYn;
 
@@ -31,13 +34,16 @@ public class Member extends BaseTimeEntity {
     private List<Order> orders = new ArrayList<>();
 
     @Builder
-    public Member(String name, String phone, String birth, String email, String username, String password, String personalInfoTermYn, String serviceTermYn) {
+    public Member(String name, String phone, String birth, String email, String code, String username, String password, String department, String emailAuthYn, String personalInfoTermYn, String serviceTermYn) {
         this.name = name;
         this.phone = phone;
         this.birth = birth;
         this.email = email;
+        this.code = code;
         this.username = username;
         this.password = password;
+        this.department = department;
+        this.emailAuthYn = emailAuthYn;
         this.personalInfoTermYn = personalInfoTermYn;
         this.serviceTermYn = serviceTermYn;
     }
