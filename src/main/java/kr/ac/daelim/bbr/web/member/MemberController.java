1750ef0f862472ad5ddc3cde4d77d3a1a99e8312
Kim Juho <sv05159@naver.com>
2021-10-06 02:38:53 +0900

    회원가입 - 이메일 인증 및 필드 추가

--- src/main/java/kr/ac/daelim/bbr/web/member/MemberController.java
+++ src/main/java/kr/ac/daelim/bbr/web/member/MemberController.java
@@ -1,14 +1,12 @@
 package kr.ac.daelim.bbr.web.member;
 
+import kr.ac.daelim.bbr.service.EmailService;
 import kr.ac.daelim.bbr.service.MemberService;
 import kr.ac.daelim.bbr.web.member.dto.MemberSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,6 +16,7 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService memberService;
+    private final EmailService emailService;
 
     @GetMapping("/add")
     public String addForm(@ModelAttribute("memberSaveRequestDto") MemberSaveRequestDto memberSaveRequestDto) {
@@ -26,11 +25,28 @@ public class MemberController {
 
     @PostMapping("/add")
     public String save(@Valid @ModelAttribute MemberSaveRequestDto memberSaveRequestDto, BindingResult bindingResult) {
-        if (bindingResult.hasErrors() || !memberSaveRequestDto.getPersonalInfoTermYn().booleanValue() || !memberSaveRequestDto.getPersonalInfoTermYn().booleanValue()) {
+        //TODO 이메일인증 false
+        if (bindingResult.hasErrors() || !memberSaveRequestDto.getPersonalInfoTermYn().booleanValue() || !memberSaveRequestDto.getServiceTermYn().booleanValue()) {
             return "members/joinForm";
         }
 
         memberService.save(memberSaveRequestDto);
         return "redirect:/login";
+    }
+
+    @PostMapping("/sendMail")
+    @ResponseBody
+    public void emailConfirm(String email) throws Exception {
+        emailService.sendSimpleMessage(email+"@email.daelim.ac.kr");
+    }
+
+    @PostMapping("/verifyCode")
+    @ResponseBody
+    public String verifyCode(String code) {
+        String result = "false";
+        if(EmailService.ePw.equals(code)) {
+            result = "true";
+        }
+        return result;
     }
 }
--- src/main/java/kr/ac/daelim/bbr/web/member/dto/MemberSaveRequestDto.java
+++ src/main/java/kr/ac/daelim/bbr/web/member/dto/MemberSaveRequestDto.java
@@ -9,26 +9,44 @@ import javax.validation.constraints.NotEmpty;
 @Getter
 public class MemberSaveRequestDto {
 
+    @NotEmpty private String name;
+    @NotEmpty private String phone;
+    @NotEmpty private String birth;
+    @NotEmpty private String email;
+    @NotEmpty private String code;
     @NotEmpty private String username;
     @NotEmpty private String password;
-    @NotEmpty private String name;
+    @NotEmpty private String department;
+    @NotEmpty private String emailAuthYn;
     private Boolean personalInfoTermYn;
     private Boolean serviceTermYn;
 
     @Builder
-    public MemberSaveRequestDto(String username, String password, String name, Boolean personalInfoTermYn, Boolean serviceTermYn) {
+    public MemberSaveRequestDto(String name, String phone, String birth, String email, String code, String username, String password, String department, String emailAuthYn, Boolean personalInfoTermYn, Boolean serviceTermYn) {
+        this.name = name;
+        this.phone = phone;
+        this.birth = birth;
+        this.email = email;
+        this.code = code;
         this.username = username;
         this.password = password;
-        this.name = name;
+        this.department = department;
+        this.emailAuthYn = emailAuthYn;
         this.personalInfoTermYn = personalInfoTermYn;
         this.serviceTermYn = serviceTermYn;
     }
 
     public Member toEntity() {
         return Member.builder()
+                .name(name)
+                .phone(phone)
+                .birth(birth)
+                .email(email)
+                .code(code)
                 .username(username)
                 .password(password)
-                .name(name)
+                .department(department)
+                .emailAuthYn(emailAuthYn)
                 .personalInfoTermYn(String.valueOf(personalInfoTermYn))
                 .serviceTermYn(String.valueOf(serviceTermYn))
                 .build();
