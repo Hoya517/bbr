26fc7cc1ff433de79c488692ed41817c7f812565
Kim Juho <sv05159@naver.com>
2021-10-18 00:47:20 +0900

    Add "/api/books/**"
    - 로그인 안했을때 홈화면 책 안보임..

--- src/main/java/kr/ac/daelim/bbr/config/WebConfig.java
+++ src/main/java/kr/ac/daelim/bbr/config/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/api/books/**",
                         "/css/**", "/img/**", "/icons/**", "/error");
     }
 }
